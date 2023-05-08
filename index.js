8460b898ce346d97dcbd24b2e82ba04da0bbac8c
vitali <vitali.slepnyov@gmail.com>
2023-05-08 23:59:28 +0300

    feat: добавить функцию debounce

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ const logHello = () => console.log('Hello!');
 const throttledLogHello = throttle(logHello, 1000);
 setInterval(throttledLogHello, 500);
 
+
+function debounce(fn, delay) {
+  let timerId;
+  return function (...args) {
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+    timerId = setTimeout(() => {
+      fn(...args);
+      timerId = undefined;
+    }, delay);
+  };
+}
+
+
