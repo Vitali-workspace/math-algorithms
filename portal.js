63f44c69c87a2700426b3181e80b0175c9c3142a
vitali <vitali.slepnyov@gmail.com>
2023-05-23 23:59:12 +0300

    feat: добавить функцию  с порталом

--- portal.js
+++ portal.js
@@ -1,6 +1,32 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+function Modal({ children }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleOpen() {
+    setIsOpen(true);
+  }
+
+  function handleClose() {
+    setIsOpen(false);
+  }
+
+  if (!isOpen) {
+    return <button onClick={handleOpen}>Open Modal</button>;
+  }
+
+  return ReactDOM.createPortal(
+    <div className="modal">
+      <div className="modal-content">
+        {children}
+        <button onClick={handleClose}>Close Modal</button>
+      </div>
+    </div>,
+    document.body
+  );
+}
+
 function App() {
   return (
     <div>
