7d413b49ca0ac1efd26691d53794f048d646d2cc
vitali <vitali.slepnyov@gmail.com>
2023-05-27 23:36:55 +0300

    feat: добавить функцию  useCounter

---
+++ debugHooks.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+function useCounter(initialValue) {
+  const [count, setCount] = useState(initialValue);
+
+
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  return [count, increment];
+}
+
+function App() {
+  const [count, increment] = useCounter(0);
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
